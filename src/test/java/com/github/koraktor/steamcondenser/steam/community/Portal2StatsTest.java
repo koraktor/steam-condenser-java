7194d07f942728e5507e13ae3b84a35f641cf230
Guto Maia <guto@guto.net>
2011-11-29 23:35:02 -0200

    removed unecessary imports left after some refactorings

--- src/test/java/com/github/koraktor/steamcondenser/steam/community/Portal2StatsTest.java
+++ src/test/java/com/github/koraktor/steamcondenser/steam/community/Portal2StatsTest.java
@@ -7,18 +7,11 @@
 
 package com.github.koraktor.steamcondenser.steam.community;
 
-import static com.github.koraktor.steamcondenser.steam.community.XMLUtil.loadXml;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
