b126fe717ab25f7738b21120650c65f87139544b
Sebastian Staudt <koraktor@gmail.com>
2011-12-05 09:55:44 +0100

    Added a test for invalid SteamGroup data

--- src/test/java/com/github/koraktor/steamcondenser/steam/community/SteamGroupTest.java
+++ src/test/java/com/github/koraktor/steamcondenser/steam/community/SteamGroupTest.java
@@ -12,16 +12,19 @@ import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.w3c.dom.Document;
-
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import org.w3c.dom.Document;
 
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -33,6 +36,9 @@ import static org.mockito.Mockito.when;
  * @author Sebastian Staudt
  */
 public class SteamGroupTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     private DocumentBuilder parser;
 
@@ -96,6 +102,26 @@ public class SteamGroupTest {
 
         assertThat(group.getCustomUrl(), is("valve"));
         assertThat(group.getBaseUrl(), is(equalTo("http://steamcommunity.com/groups/valve")));
+    }
+
+    @Test
+    public void testInvalidXml() throws Exception {
+        this.expectedException.expect(is(instanceOf(SteamCondenserException.class)));
+        this.expectedException.expectMessage("XML data could not be parsed.");
+
+        Document memberDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(this.getClass().getResourceAsStream("invalid.xml"));
+        when(this.parser.parse("http://steamcommunity.com/groups/valve/memberslistxml?p=1")).thenReturn(memberDocument);
+
+        SteamGroup group = new SteamGroup("valve", true);
+        ArrayList<SteamId> members = group.getMembers();
+
+        assertThat(group.getMemberCount(), is(221));
+        assertThat(members.get(0).getSteamId64(), is(76561197960265740L));
+        assertFalse(members.get(0).isFetched());
+        assertThat(members.get(members.size() - 1).getSteamId64(), is(76561197970323416L));
+        assertTrue(group.isFetched());
+
+        verify(this.parser).parse("http://steamcommunity.com/groups/valve/memberslistxml?p=1");
     }
 
     @Test
