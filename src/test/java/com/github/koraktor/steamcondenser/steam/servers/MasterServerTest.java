75b7bf863f8fce007048dff6c13a6d1b2a8517d2
Sebastian Staudt <koraktor@gmail.com>
2013-02-08 01:22:44 +0100

    Fixed auto-retry for master server queries
    Fixes koraktor/steam-condenser#196

--- src/test/java/com/github/koraktor/steamcondenser/steam/servers/MasterServerTest.java
+++ src/test/java/com/github/koraktor/steamcondenser/steam/servers/MasterServerTest.java
@@ -161,6 +161,61 @@ public class MasterServerTest {
     }
 
     @Test
+    public void testGetServersSwapIp() throws Exception {
+        M2A_SERVER_BATCH_Paket packet1 = mock(M2A_SERVER_BATCH_Paket.class);
+        Vector<String> servers1 = new Vector<String>();
+        servers1.add("127.0.0.1:27015");
+        servers1.add("127.0.0.2:27015");
+        servers1.add("127.0.0.3:27015");
+        when(packet1.getServers()).thenReturn(servers1);
+        M2A_SERVER_BATCH_Paket packet2 = mock(M2A_SERVER_BATCH_Paket.class);
+        Vector<String> servers2 = new Vector<String>();
+        servers2.add("127.0.0.4:27015");
+        servers2.add("0.0.0.0:0");
+        when(packet2.getServers()).thenReturn(servers2);
+        when(this.server.socket.getReply())
+            .thenReturn(packet1)
+            .thenThrow(new TimeoutException())
+            .thenThrow(new TimeoutException())
+            .thenThrow(new TimeoutException())
+            .thenReturn(packet2);
+
+        when(this.server.rotateIp()).thenReturn(false);
+
+        Vector<InetSocketAddress> servers = new Vector<InetSocketAddress>();
+        servers.add(new InetSocketAddress("127.0.0.1", 27015));
+        servers.add(new InetSocketAddress("127.0.0.2", 27015));
+        servers.add(new InetSocketAddress("127.0.0.3", 27015));
+        servers.add(new InetSocketAddress("127.0.0.4", 27015));
+
+        assertThat(this.server.getServers(MasterServer.REGION_EUROPE, "filter"), is(equalTo(servers)));
+
+        verify(this.server, times(3)).rotateIp();
+        verify(this.server.socket).send(argThat(new BaseMatcher<A2M_GET_SERVERS_BATCH2_Paket>() {
+            public boolean matches(Object o) {
+                if(!(o instanceof A2M_GET_SERVERS_BATCH2_Paket)) {
+                    return false;
+                }
+                A2M_GET_SERVERS_BATCH2_Paket packet = (A2M_GET_SERVERS_BATCH2_Paket) o;
+                return Arrays.equals(packet.getBytes(), "\u0031\u00030.0.0.0:0\0filter\0".getBytes());
+            }
+
+            public void describeTo(Description description) {}
+        }));
+        verify(this.server.socket, times(4)).send(argThat(new BaseMatcher<A2M_GET_SERVERS_BATCH2_Paket>() {
+            public boolean matches(Object o) {
+                if(!(o instanceof A2M_GET_SERVERS_BATCH2_Paket)) {
+                    return false;
+                }
+                A2M_GET_SERVERS_BATCH2_Paket packet = (A2M_GET_SERVERS_BATCH2_Paket) o;
+                return Arrays.equals(packet.getBytes(), "\u0031\u0003127.0.0.3:27015\0filter\0".getBytes());
+            }
+
+            public void describeTo(Description description) {}
+        }));
+    }
+
+    @Test
     public void testGetServersTimeout() throws Exception {
         int retries = new Random().nextInt(4) + 1;
         MasterServer.setRetries(retries);
